8e7bb13f1ad2ecc60122c86de3ddbe487febce8a
Pedro Markun <pedro@markun.com.br>
2016-09-03 20:11:58 -0300

    mais mudancas no html

--- quiz/static/js/main.js
+++ quiz/static/js/main.js
@@ -2,6 +2,12 @@ var COMPARA = [1,1,1,1,1,1,1,1,1,1]
 var MEUS_VOTOS = [];
 var s = 0;
 $(document).ready(function () {
+	$(".seta").click(function (){
+		$('html, body').animate({
+    		scrollTop: $("#main").offset().top
+		}, 1000);
+	});
+
 	$(".controle .btn").click(function (e){
 		var q = Number($(e.currentTarget).data('questao'));
 		var voto = $(e.currentTarget).data('voto')
