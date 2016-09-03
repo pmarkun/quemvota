e8a75b65dc73060bea8c0fafd4e299118f2cbab1
Pedro Markun <pedro@markun.com.br>
2016-09-03 19:33:39 -0300

    template, javascript e candidato

--- quiz/static/js/main.js
+++ quiz/static/js/main.js
@@ -1,1 +1,24 @@
-
+var COMPARA = [1,1,1,1,1,1,1,1,1,1]
+var MEUS_VOTOS = [];
+var s = 0;
+$(document).ready(function () {
+	$(".controle .btn").click(function (e){
+		var q = Number($(e.currentTarget).data('questao'));
+		var voto = $(e.currentTarget).data('voto')
+		MEUS_VOTOS.push(voto);
+		if (q < $(".questao").length) 
+		{
+			$(".questao").hide('slow');
+			$("#q"+(q+1)).show('slow');
+		}
+		else {
+			for (i=0;i<MEUS_VOTOS.length;i++) {
+				if (MEUS_VOTOS[i] == COMPARA[i]) {
+					s += 1;
+				}
+			}
+			var semelhanca = s/MEUS_VOTOS.length;
+			alert(semelhanca.toPrecision(2)*100 + "% de semelhança");
+		}
+	});
+});
--- quiz/views.py
+++ quiz/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from quiz.models import Proposicao
 
+from .forms import CandidatoForm
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return render_to_response('index.html', {})
+	props = Proposicao.objects.filter()[0:10]
+	return render_to_response('index.html', {'props' : props})
+
+def candidato(request):
+	form = CandidatoForm()
+	return render_to_response('candidato.html', {'form' : form})
